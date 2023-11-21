2dcba86c92cfe51e72e81a60e13ae204a592561e
Kacper Misterski <36102665+KCPR13@users.noreply.github.com>
2023-11-21 16:37:30 +0100

    add openCV

--- app/src/main/java/pl/kacper/misterski/walldrill/domain/ColorAnalyzer.kt
+++ app/src/main/java/pl/kacper/misterski/walldrill/domain/ColorAnalyzer.kt
@@ -5,8 +5,11 @@ import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import pl.kacper.misterski.walldrill.domain.enums.AnalyzerMode
 import pl.kacper.misterski.walldrill.domain.models.AnalyzerResult
+import kotlin.math.abs
+import android.graphics.Color as AndroidColor
 
 
 class ColorAnalyzer(private val analyzerMode: AnalyzerMode) : ImageAnalysis.Analyzer {
@@ -16,7 +19,7 @@ class ColorAnalyzer(private val analyzerMode: AnalyzerMode) : ImageAnalysis.Anal
     private var onColorDetected: ((AnalyzerResult) -> Unit)? = null
     private var colorToDetect: Color? = null
     fun init(
-        colorToDetect: Color?=null,
+        colorToDetect: Color? = null,
         onColorDetected: ((AnalyzerResult) -> Unit)?,
     ) {
         this.onColorDetected = onColorDetected
@@ -31,7 +34,7 @@ class ColorAnalyzer(private val analyzerMode: AnalyzerMode) : ImageAnalysis.Anal
         val bitmap = image.toBitmap()
 
 
-       val result =  when (analyzerMode) {
+        val result = when (analyzerMode) {
             AnalyzerMode.AIM -> analyzeForAimMode(bitmap)
             AnalyzerMode.DETECTION -> analyzeForDetectionMode(bitmap)
         }
@@ -42,20 +45,46 @@ class ColorAnalyzer(private val analyzerMode: AnalyzerMode) : ImageAnalysis.Anal
     }
 
 
-    private fun isColorDetected(pixelColor: Color, colorToDetect: Color): Boolean {
-        return pixelColor.red == colorToDetect.red &&
-                pixelColor.green == colorToDetect.green &&
-                pixelColor.blue == colorToDetect.blue
+    private fun isColorDetected(pixelColor: Color, prevPixelColor: Color?): Boolean {
+
+//
+//        return pixelColor.red == colorToDetect.red &&
+//                pixelColor.green == colorToDetect.green &&
+//                pixelColor.blue == colorToDetect.blue
+
+        val threshold = 0.02f
+        val argb = pixelColor.toArgb()
+
+        // Convert Color to luminance (grayscale)
+        val luminance = (
+                0.299 * AndroidColor.red(argb) +
+                        0.587 * AndroidColor.green(argb) +
+                        0.114 * AndroidColor.blue(argb)
+                ) / 255.0
+
+      if (prevPixelColor == null) return false
+        val prevArgb = prevPixelColor.toArgb()
+        val prevLuminance = (
+                0.299 * AndroidColor.red(prevArgb) +
+                        0.587 * AndroidColor.green(prevArgb) +
+                        0.114 * AndroidColor.blue(prevArgb)
+                ) / 255.0
+
+        val diff = abs(luminance - prevLuminance)
+
+        // Check if the luminance crosses the specified threshold
+        return diff > threshold && (prevPixelColor.isBlackOrWhite() && pixelColor.isBlackOrWhite())
+
     }
 
     private fun analyzeForDetectionMode(bitmap: Bitmap): AnalyzerResult {
         val x = bitmap.width / 2
         val y = bitmap.height / 2
         val centerPixelColor = bitmap.getPixel(x, y)
-        return AnalyzerResult(Color(centerPixelColor), listOf(Pair(x,y)))
+        return AnalyzerResult(Color(centerPixelColor), listOf(Pair(x, y)))
     }
 
-    private fun analyzeForAimMode(bitmap: Bitmap): AnalyzerResult?{
+    private fun analyzeForAimMode(bitmap: Bitmap): AnalyzerResult? {
         val colorToDetect = colorToDetect ?: return null
 
         val width = bitmap.width
@@ -65,20 +94,37 @@ class ColorAnalyzer(private val analyzerMode: AnalyzerMode) : ImageAnalysis.Anal
         var detectedPixelsCount = 0
         val detectedLocations = mutableListOf<Pair<Int, Int>>()
 
+
+        var prevPixelColor: Color? = null
         for (y in 0 until height) {
             for (x in 0 until width) {
                 val pixel = bitmap.getPixel(x, y)
                 val color = Color(pixel)
 
-                if (isColorDetected(color, colorToDetect)) {
+                if (isColorDetected(color, prevPixelColor)) {
                     detectedPixelsCount++
                     detectedLocations.add(Pair(x, y))
                 }
+                prevPixelColor = color
             }
         }
         Log.d(TAG, "detectedPoints: ${detectedLocations.size}")
 
-        return AnalyzerResult(colorToDetect,detectedLocations)
+        return AnalyzerResult(colorToDetect, detectedLocations)
     }
+}
+
+fun Color.isBlackOrWhite( blackThreshold: Float = 0.1f, whiteThreshold: Float = 0.95f): Boolean {
+    val argb = this.toArgb()
+
+    // Calculate luminance (grayscale)
+    val luminance = (
+            0.299 * AndroidColor.red(argb) +
+                    0.587 * AndroidColor.green(argb) +
+                    0.114 * AndroidColor.blue(argb)
+            ) / 255.0
+
+    // Check if the color is black or white based on the thresholds
+    return luminance <= blackThreshold || luminance >= whiteThreshold
 }
 
