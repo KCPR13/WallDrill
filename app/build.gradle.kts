7bbecb1be62ae8cf819275aab01fcbb98621125f
KCPR13 <kacper.misterski13@gmail.com>
2024-07-22 20:41:50 +0200

    apply spotless to subprojects

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,152 +1,152 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.android.kotlin)
-    alias(libs.plugins.android.kapt)
-    alias(libs.plugins.android.ksp)
-    alias(libs.plugins.android.hilt)
-    alias(libs.plugins.spotless)
-    alias(libs.plugins.ktlint)
+	alias(libs.plugins.android.application)
+	alias(libs.plugins.android.kotlin)
+	alias(libs.plugins.android.kapt)
+	alias(libs.plugins.android.ksp)
+	alias(libs.plugins.android.hilt)
+	alias(libs.plugins.spotless)
+	alias(libs.plugins.ktlint)
 }
 
 android {
 
-    val appId = "pl.kacper.misterski.walldrill"
+	val appId = "pl.kacper.misterski.walldrill"
 
-    namespace = appId
-    compileSdk =
-        libs.versions.sdk
-            .get()
-            .toInt()
+	namespace = appId
+	compileSdk =
+		libs.versions.sdk
+			.get()
+			.toInt()
 
-    defaultConfig {
-        applicationId = appId
-        minSdk =
-            libs.versions.minSdk
-                .get()
-                .toInt()
-        targetSdk =
-            libs.versions.sdk
-                .get()
-                .toInt()
-        versionCode =
-            libs.versions.versionCode
-                .get()
-                .toInt()
-        versionName = libs.versions.versionName.get()
+	defaultConfig {
+		applicationId = appId
+		minSdk =
+			libs.versions.minSdk
+				.get()
+				.toInt()
+		targetSdk =
+			libs.versions.sdk
+				.get()
+				.toInt()
+		versionCode =
+			libs.versions.versionCode
+				.get()
+				.toInt()
+		versionName = libs.versions.versionName.get()
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		vectorDrawables {
+			useSupportLibrary = true
+		}
 
-        javaCompileOptions {
-            annotationProcessorOptions {
-                arguments +=
-                    mapOf(
-                        "room.schemaLocation" to "$projectDir/schemas",
-                        "room.incremental" to "true",
-                    )
-            }
-        }
-    }
+		javaCompileOptions {
+			annotationProcessorOptions {
+				arguments +=
+					mapOf(
+						"room.schemaLocation" to "$projectDir/schemas",
+						"room.incremental" to "true",
+					)
+			}
+		}
+	}
 
-    sourceSets {
-        getByName("androidTest") {
-            res.srcDirs("src/res")
-        }
-    }
+	sourceSets {
+		getByName("androidTest") {
+			res.srcDirs("src/res")
+		}
+	}
 
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-        getByName("debug") {
-            isDebuggable = true
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+	buildTypes {
+		getByName("release") {
+			isMinifyEnabled = true
+			isShrinkResources = true
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro",
+			)
+		}
+		getByName("debug") {
+			isDebuggable = true
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
+	}
 
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtensionVersion.get()
-    }
-    packaging {
-        // TODO K remove?
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+	buildFeatures {
+		compose = true
+	}
+	composeOptions {
+		kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtensionVersion.get()
+	}
+	packaging {
+		// TODO K remove?
+		resources {
+			excludes += "/META-INF/{AL2.0,LGPL2.1}"
+		}
+	}
 }
 
 dependencies {
 
-    implementation(project(":openCV"))
+	implementation(project(":openCV"))
 
-    implementation(libs.androidx.ktx)
-    implementation(libs.lifecycle)
+	implementation(libs.androidx.ktx)
+	implementation(libs.lifecycle)
 
 //    Camera
-    implementation(libs.androidx.camera.view)
-    implementation(libs.androidx.camera.lifecycle)
-    implementation(libs.androidx.camera.camera2)
+	implementation(libs.androidx.camera.view)
+	implementation(libs.androidx.camera.lifecycle)
+	implementation(libs.androidx.camera.camera2)
 
 //    Hilt
-    implementation(libs.hilt)
-    kapt(libs.hiltKapt)
+	implementation(libs.hilt)
+	kapt(libs.hiltKapt)
 
 //    Room
-    implementation(libs.android.room.ktx)
-    implementation(libs.android.room.runtime)
-    ksp(libs.android.roomKapt)
+	implementation(libs.android.room.ktx)
+	implementation(libs.android.room.runtime)
+	ksp(libs.android.roomKapt)
 
 //    Compose
-    implementation(libs.androidx.compose.navigation)
-    implementation(libs.androidx.compose.hiltNavigation)
-    implementation(libs.androidx.compose.activity)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.graphics)
-    implementation(libs.androidx.compose.material)
-    implementation(libs.androidx.compose.preview)
-    implementation(platform(libs.androidx.compose.composeBom))
+	implementation(libs.androidx.compose.navigation)
+	implementation(libs.androidx.compose.hiltNavigation)
+	implementation(libs.androidx.compose.activity)
+	implementation(libs.androidx.compose.ui)
+	implementation(libs.androidx.compose.graphics)
+	implementation(libs.androidx.compose.material)
+	implementation(libs.androidx.compose.preview)
+	implementation(platform(libs.androidx.compose.composeBom))
 
 //   Tests
-    testImplementation(libs.junit.core)
+	testImplementation(libs.junit.core)
 
-    androidTestImplementation(libs.junit.ext)
-    androidTestImplementation(libs.expresso.core)
-    androidTestImplementation(libs.junit.rules)
-    androidTestImplementation(libs.junit.compose.test)
-    androidTestImplementation(libs.navigation.test)
+	androidTestImplementation(libs.junit.ext)
+	androidTestImplementation(libs.expresso.core)
+	androidTestImplementation(libs.junit.rules)
+	androidTestImplementation(libs.junit.compose.test)
+	androidTestImplementation(libs.navigation.test)
 
-    debugImplementation(libs.compose.test.manifest)
-    debugImplementation(libs.compose.test.tooling)
+	debugImplementation(libs.compose.test.manifest)
+	debugImplementation(libs.compose.test.tooling)
 }
 
 // TODO K move to separate file
 spotless {
-    val ktlintVersion = "0.48.0"
-    kotlin {
-        target("**/*.kt")
-        targetExclude("**/build/**/*.kt")
-        ktlint(ktlintVersion)
-            .setEditorConfigPath("$rootDir/.editorconfig")
-        trimTrailingWhitespace()
-        indentWithSpaces()
-        endWithNewline()
-        licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
-    }
-    format("kts") {
-        target("**/*.kts")
-        targetExclude("**/build/**/*.kts")
-    }
+	val ktlintVersion = "0.48.0"
+	kotlin {
+		target("**/*.kt")
+		targetExclude("**/build/**/*.kt")
+		ktlint(ktlintVersion)
+			.setEditorConfigPath("$rootDir/.editorconfig")
+		trimTrailingWhitespace()
+		indentWithSpaces()
+		endWithNewline()
+		licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
+	}
+	format("kts") {
+		target("**/*.kts")
+		targetExclude("**/build/**/*.kts")
+	}
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.android.kotlin) apply false
-    alias(libs.plugins.android.hilt) apply false
-    alias(libs.plugins.android.kapt) apply false
-    alias(libs.plugins.android.ksp) apply false
-    alias(libs.plugins.version.catalog.update)
-    alias(libs.plugins.gradle.versions)
-    alias(libs.plugins.spotless)
+	alias(libs.plugins.android.application) apply false
+	alias(libs.plugins.android.library) apply false
+	alias(libs.plugins.android.kotlin) apply false
+	alias(libs.plugins.android.hilt) apply false
+	alias(libs.plugins.android.kapt) apply false
+	alias(libs.plugins.android.ksp) apply false
+	alias(libs.plugins.version.catalog.update)
+	alias(libs.plugins.gradle.versions)
+	alias(libs.plugins.spotless)
 }
 
 apply("${project.rootDir}/scripts/gradle-update.gradle")
+
+subprojects {
+	afterEvaluate {
+		tasks.named("preBuild") {
+			dependsOn("spotlessApply")
+		}
+	}
+}
