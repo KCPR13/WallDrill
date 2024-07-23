3796ea1b4a8914cf7e3148e384a85f3cc252f0c0
KCPR13 <kacper.misterski13@gmail.com>
2024-07-23 19:08:03 +0200

    integration with detekt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	alias(libs.plugins.android.hilt)
 	alias(libs.plugins.spotless)
 	alias(libs.plugins.ktlint)
+	alias(libs.plugins.detekt)
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
 	alias(libs.plugins.android.application) apply false
@@ -9,6 +12,7 @@ plugins {
 	alias(libs.plugins.version.catalog.update)
 	alias(libs.plugins.gradle.versions)
 	alias(libs.plugins.spotless)
+	alias(libs.plugins.detekt)
 }
 
 apply("${project.rootDir}/scripts/gradle-update.gradle")
@@ -19,4 +23,31 @@ subprojects {
 			dependsOn("spotlessApply")
 		}
 	}
+
+	tasks.withType<Detekt>().configureEach {
+		jvmTarget = "1.8"
+	}
+	tasks.withType<DetektCreateBaselineTask>().configureEach {
+		jvmTarget = "1.8"
+	}
+
+	tasks.withType<Detekt>().configureEach {
+		reports {
+			xml {
+				required.set(true)
+				outputLocation.set(file("build/reports/detekt/detekt-report.xml"))
+			}
+			html {
+				required.set(true)
+				outputLocation.set(file("build/reports/detekt/detekt-report.html"))
+			}
+		}
+	}
+}
+
+detekt {
+	buildUponDefaultConfig = true
+	allRules = false
+	config.setFrom("$projectDir/config/detekt.yml")
+	baseline = file("$projectDir/config/baseline.xml")
 }
